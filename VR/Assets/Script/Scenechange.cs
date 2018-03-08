9026bad03dbddd57a83b0124f673336bfec52196
daimenion <daimenion@hotmail.com>
2018-03-08 17:45:24 -0500

    vr

---
+++ VR/Assets/Script/Scenechange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenechange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void loadScene(int SceneIndex){
+
+		SceneManager.LoadScene (SceneIndex);
+	}
+
+}
