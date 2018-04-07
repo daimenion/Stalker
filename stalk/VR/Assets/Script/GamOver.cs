a80f7bf9a0cc040736c995d73721dd6c6f702afc
daimenion <daimenion@hotmail.com>
2018-04-06 22:22:40 -0400

    added a text

--- stalk/VR/Assets/Script/GamOver.cs
+++ stalk/VR/Assets/Script/GamOver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GamOver : MonoBehaviour {
 
@@ -9,20 +10,26 @@ public class GamOver : MonoBehaviour {
 	Animator anim;
 	public GameObject doo;
 	public GameObject dor;
+	[SerializeField] private Text customText;
     // Use this for initialization
     void Start()
     {
 		anim = GetComponent<Animator> ();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+		if (Keys.keycount == 4 )
+			customText.enabled = true;
+		else
+			customText.enabled = false;
     }
 
     void OnTriggerEnter(Collider c)
     {
+
 		if (Keys.keycount == 4 && c.gameObject.tag == "Player") {
 			anim.SetBool ("open", true);
 			anim.SetTrigger ("opens");
